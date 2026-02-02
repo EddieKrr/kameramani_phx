56c08c3d7d464c6f8ec79c869743ab6ed106c909
EddieKrr <166714763+EddieKrr@users.noreply.github.com>
2026-02-02 14:57:22 +0300

    Created users table

--- priv/repo/migrations/20260202113435_create_users.exs
+++ priv/repo/migrations/20260202113435_create_users.exs
@@ -2,6 +2,11 @@ defmodule KameramaniPhx.Repo.Migrations.CreateUsers do
   use Ecto.Migration
 
   def change do
-
+    create table(:users) do
+    add :name, :string, null: false
+    add :username, :string, null: false
+    add :email, :string, required: true
+    add :age, :integer, default: 0
+    end
   end
 end
