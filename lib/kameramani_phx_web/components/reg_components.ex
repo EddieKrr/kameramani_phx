ffbe1ff9f7ecd8b6abb64598988945aab538b236
EddieKrr <166714763+EddieKrr@users.noreply.github.com>
2026-02-04 09:33:40 +0300

    Login form placeholder

--- lib/kameramani_phx_web/components/reg_components.ex
+++ lib/kameramani_phx_web/components/reg_components.ex
@@ -7,8 +7,23 @@ defmodule RegComponents do
 
   def mesage(assigns) do
     ~H"""
-      <input type="text" id={@field.id} name={@field.name} {@rest} class="border-2 border-sky-950 rounded-lg focus:outline-none"/>
-      <div :for={msg <- @field.errors}><%=msg%></div>
+      <input type="text" id={@field.id} name={@field.name} {@rest} class="border-2 border-indigo-700 rounded-3xl focus:outline-none justify-center items-center"/>
+      <div :for={msg <- @field.errors} class="text-red-500"><%=msg%></div>
+    """
+  end
+end
+
+defmodule LogComponents do
+  use KameramaniPhxWeb, :html
+
+  attr :field, Phoenix.HTML.FormField, required: true
+  attr :type, :string, default: "text"
+  attr :rest, :global, include: ~w(placeholder type username password)
+
+  def log(assigns) do
+    ~H"""
+    <input type="text" id={@field.id} username={@field.name} {@rest} class="rounded-full border-2 border-sky-500 focus:outline-none text-slate-800"/>
+    <div :for={msg <- @field.errors} class="text-red-500"><%=msg%></div>
     """
   end
 end
--- lib/kameramani_phx_web/live/auth_live.ex
+++ lib/kameramani_phx_web/live/auth_live.ex
@@ -1,6 +1,7 @@
 defmodule KameramaniPhxWeb.AuthLive do
   use KameramaniPhxWeb, :live_view
   import RegComponents
+  import LogComponents
 
   @empty_reg%{
     "name" => "",
@@ -10,10 +11,16 @@ defmodule KameramaniPhxWeb.AuthLive do
     "password" => ""
   }
 
+  @empty_log%{
+    "username" => "",
+    "password" => ""
+  }
 
   def mount(_params, _session, socket) do
     reg_form=to_form(@empty_reg, as: :reg)
-    {:ok, assign(socket, reg_form: reg_form)}
+    log_form=to_form(@empty_log, as: :log)
+    {:ok, assign(socket, reg_form: reg_form, log_form: log_form)}
+
   end
 
   def handle_event("validate_entry", %{"reg"=>form_msg}, socket) do
@@ -65,11 +72,8 @@ defmodule KameramaniPhxWeb.AuthLive do
 
   def render(assigns) do
     ~H"""
-    <div class="container group mx-auto my-18 relative w-[768px] max-w-full min-h-[480px] bg-indigo-200 rounded-[30px] shadow-2xl overflow-hidden">
-      <div class="form-login">
-
-      </div>
-      <div class ="form-register flex w-1/2 pt-22">
+    <div class="flex flex-row container group mx-auto my-18 relative w-[768px] max-w-full min-h-[480px] bg-indigo-200 rounded-[30px] shadow-2xl overflow-hidden">
+      <div class ="form-register flex w-1/2">
         <.form
           for={@reg_form}
           phx-change="validate_entry"
@@ -86,17 +90,30 @@ defmodule KameramaniPhxWeb.AuthLive do
         </.form>
 
       </div>
+      <div class="form-login flex w-1/2 ">
+        <.form
+          for={@log_form}
+          phx-change="validate_log"
+          phx-submit="login"
+          class="flex flex-col m-auto gap-2"
+        >
+        <.log field={@log_form[:name]} placeholder=" Username"/>
+        <.log field={@log_form[:password]} placeholder=" Password"/>
+
+        <button type="submit" class="rounded-full bg-sky-500 p-2">Login</button>
+        </.form>
+      </div>
     <div class="toggle-box absolute top-0 left-1/2 w-full h-full z-10 overflow-hidden transition-all duration-[600ms] ease-in-out group-[.active]:-translate-x-full rounded-l-[150px] group-[.active]:rounded-l-none group-[.active]:rounded-r-[150px]">
         <div class="absolute h-full w-[300%] left-[-100%] bg-gradient-to-r from-sky-500 via-indigo-950 to-slate-950 text-white transition-transform duration-[600ms] ease-in-out group-[.active]:translate-x-1/2">
         </div>
         <div class="toggle-panel login absolute w-1/2 h-full flex flex-col justify-center items-center px-8 text-center top-0 transition-all duration-600 ease-in-out left-0 group-[.active]:-left-1/2 delay-300">
-            <h1 class="text-xl font-bold">Not Part of the Family?</h1>
-            <p class="mb-5">Go ahead and register</p>
-            <button phx-click={JS.add_class("active", to: ".container")} class="log-btn w-40 h-11 bg-transparent rounded-full border-2 border-white shadow-none">Sign Up</button>
-        </div>
-        <div class="toggle-panel register absolute w-1/2 h-full flex flex-col justify-center items-center px-8 text-center top-0 transition-all duration-600 ease-in-out -right-1/2 group-[.active]:right-0 group-[.active]:delay-300">
             <h1 class="text-xl font-bold">Hii Uso ni Familiar</h1>
             <p class="mb-5">Go ahead and login</p>
+            <button phx-click={JS.add_class("active", to: ".container")} class="log-btn w-40 h-11 bg-transparent rounded-full border-2 border-white shadow-none">Log In</button>
+        </div>
+        <div class="toggle-panel register absolute w-1/2 h-full flex flex-col justify-center items-center px-8 text-center top-0 transition-all duration-600 ease-in-out -right-1/2 group-[.active]:right-0 group-[.active]:delay-300">
+            <h1 class="text-xl font-bold">Not Part of the Family?</h1>
+            <p class="mb-5">Go ahead and register</p>
             <button phx-click={JS.remove_class("active", to: ".container")} class="reg-btn w-40 h-11 bg-transparent rounded-full border-2 border-white shadow-none">Sign Up</button>
         </div>
       </div>
