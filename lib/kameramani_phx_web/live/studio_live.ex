14be9ef857252f12090ecc69e0239d5d69b0b018
EddieKrr <166714763+EddieKrr@users.noreply.github.com>
2026-02-09 22:01:11 +0300

    Resolve mix.lock conflicts

--- lib/kameramani_phx_web/live/studio_live.ex
+++ lib/kameramani_phx_web/live/studio_live.ex
@@ -1,3 +1,18 @@
-defmodule KameramaniPhxWeb.StudioLive do
-  
-end
+# defmodule KameramaniPhxWeb.StudioLive do
+#   strmr_data = %{title: "", game: "", stream_key: "" }
+
+#   def mount(_params, _session, socket) do
+#     # tst_data = [@strmr_data.title["John"], @strmr_data.game["Skate 2"], @strmr_data.stream_key["ST-01"]]
+#     # tst = to_form(@strmr_data as: :std_test)
+#     {:noreply, socket}
+#   end
+
+#   def render(assign) do
+#     ~H"""
+#     <div class="lg: grid-cols-3">
+#       <div class="col-span-2"></div>
+#       <div class="col-start-3"></div>
+#     </div>
+#     """
+#   end
+# end
