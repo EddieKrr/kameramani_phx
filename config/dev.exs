36569348239cfda09cbe968f8a264953101b8504
EddieKrr <166714763+EddieKrr@users.noreply.github.com>
2026-02-11 12:31:22 +0300

    local postgres bypass

--- config/dev.exs
+++ config/dev.exs
@@ -92,3 +92,6 @@ config :phoenix_live_view,
 
 # Disable swoosh api client as it is only required for production adapters.
 config :swoosh, :api_client, false
+if File.exists?("config/dev.local.exs") do
+  import_config "dev.local.exs"
+end
