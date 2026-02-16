8aeba07a123e5a389222befc298335fc17d7d0ce
DTech <dtech@D-Tech.D-Tech.com>
2026-02-16 10:46:14 +0300

    stream is up and runnig

--- lib/kameramani_phx_web/live/Streaming/pipeline.ex
+++ lib/kameramani_phx_web/live/Streaming/pipeline.ex
@@ -1,56 +1,51 @@
-# defmodule KameramaniPhxWeb.Streaming.Pipeline do
-#   use Membrane.Pipeline
-#   import Membrane.ChildrenSpec
-#   import Membrane.Pad # New import
+defmodule KameramaniPhxWeb.Streaming.Pipeline do
+  use Membrane.Pipeline
+  import Membrane.ChildrenSpec
+  import Membrane.Pad # New import
 
-#   # These aliases ensure the compiler knows exactly where to look
-#   alias Membrane.RTMP.SourceBin
-#   alias Membrane.H264.Parser, as: H264Parser
-#   alias Membrane.HTTPAdaptiveStream.SinkBin
-#   alias Membrane.HTTPAdaptiveStream.HLS
-#   alias Membrane.HTTPAdaptiveStream.Storages.FileStorage, as: FileStorage
+  # These aliases ensure the compiler knows exactly where to look
+  alias Membrane.RTMP.SourceBin
+  alias Membrane.H264.Parser, as: H264Parser
+  alias Membrane.HTTPAdaptiveStream.SinkBin
+  alias Membrane.HTTPAdaptiveStream.HLS
+  alias Membrane.HTTPAdaptiveStream.Storages.FileStorage, as: FileStorage
 
-#   def start() do
-#     start_link(:my_pipeline, [])
-#   end
+  def start() do
+    start_link(:my_pipeline, [])
+  end
 
-#   def start_link(name, opts) do
-#     Membrane.Pipeline.start_link(__MODULE__, name, opts)
-#   end
+  def start_link(name, opts) do
+    Membrane.Pipeline.start_link(__MODULE__, name, opts)
+  end
 
-#   @impl true
-#   def handle_init(_ctx, _opts) do
-#     spec = [
-#       child(:src, %SourceBin{
-#         url: "rtmp://127.0.0.1:1935/live/cube_test"
-#       }),
-#       child(:parser, %H264Parser{output_alignment: :au}), # This parser is for H264 video
-#       child(:sink, %SinkBin{
-#         manifest_module: HLS,
-#         target_window_duration: :infinity,
-#         persist?: false,
-#         storage: %FileStorage{directory: "priv/static/live/cube"}
-#       }),
+  @impl true
+  def handle_init(_ctx, _opts) do
+    spec = [
+      # Audio path: src:audio -> sink:input(audio)
+      child(:src, %SourceBin{
+        url: "rtmp://127.0.0.1:1935/live/cube_test"
+      })
+      |> via_out(:audio)
+      |> via_in(Pad.ref(:input, :audio),
+        options: [encoding: :AAC, segment_duration: Membrane.Time.seconds(4)]
+      )
+      |> child(:sink, %SinkBin{
+        manifest_module: HLS,
+        target_window_duration: :infinity,
+        persist?: false,
+        storage: %FileStorage{directory: "priv/static/live/cube"}
+      }),
 
-#       # Link video path: src:video -> parser:input -> sink:input(video)
-#       link(
-#         {:src, :video},
-#         {:parser, :input}
-#       ),
-#       link(
-#         {:parser, :output},
-#         {:sink, Pad.ref(:input, :video)}, # Specify input pad for video in sink
-#         options: [encoding: :H264, segment_duration: Membrane.Time.seconds(4)]
-#       ),
+      # Video path: src:video -> parser:input -> sink:input(video)
+      get_child(:src)
+      |> via_out(:video)
+      |> child(:parser, %H264Parser{output_alignment: :au})
+      |> via_in(Pad.ref(:input, :video),
+        options: [encoding: :H264, segment_duration: Membrane.Time.seconds(4)]
+      )
+      |> get_child(:sink)
+    ]
 
-#       # Link audio path: src:audio -> sink:input(audio)
-#       link(
-#         {:src, :audio},
-#         {:sink, Pad.ref(:input, :audio)}, # Specify input pad for audio in sink
-#         options: [encoding: :AAC, segment_duration: Membrane.Time.seconds(4)]
-#       )
-#     ]
-
-#     {[spec: spec], %{}}
-#   end
-# end
+    {[spec: spec], %{}}
+  end
+end
